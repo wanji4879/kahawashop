9afc3f6708088a30c002a0c5578e722c3e3cd9e0
wanji4879 <wanjirulovelyne93@gmail.com>
2024-09-29 15:59:42 +0300

    New commit message

--- kahawaapp/models.py
+++ kahawaapp/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+from django.contrib import admin
 
 
 class Kahawaapp(models.Model):
-  name= models.CharField(max_length=255)
-  price = models.FloatField()
-  Quantity = models.IntegerField()
-  image =models.CharField(max_length=2083)
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    quantity = models.PositiveIntegerField()
+    image = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
 
   
--- kahawaapp/views.py
+++ kahawaapp/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Kahawaapp  
+from .models import Kahawaapp
 
-
-
-def home(request): 
-    kahawaapp_data = Kahawaapp.objects.all() 
-    return render(request, 'home.html',{'kahawaapp_data': kahawaapp_data})
+def home(request):
+    kahawaapp_data = Kahawaapp.objects.all()
+    return render(request, 'home.html', {'kahawaapp_data': kahawaapp_data})
